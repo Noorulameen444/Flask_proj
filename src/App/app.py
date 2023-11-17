3323f9a4676a9f8e123f5d7d0c86026dd7cafa9f
noor146444 <noorulamean146@gmail.com>
2023-11-17 09:23:45 +0530

    Modify src

--- src/App/app.py
+++ src/App/app.py
@@ -4,14 +4,17 @@ from src.Logging.logger import custom_logger
 logger = custom_logger(__file__)
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     logger.info('user has reached index page')
     return render_template('index.html')
 
+
 # @app.route('/<usr_name>')
 def user(usr_name):
     logger.info(f'{usr_name} has accessed their personal space.')
-    return render_template('user.html', name = usr_name)
+    return render_template('user.html', name=usr_name)
 
-app.add_url_rule('/<usr_name>','user page',user)
+
+app.add_url_rule('/<usr_name>', 'user page', user)
